7f6b9fa3c929fed792178d6d498a1f24454bcabe
Kumar <pradeep-kumar@outlook.com>
2025-12-13 23:17:16 +0530

    basic server

--- app/main.py
+++ app/main.py
@@ -5,10 +5,18 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # TODO: Uncomment the code below to pass the first stage
-    #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    host = "localhost"
+    port = 4221
+
+    with socket.create_server((host, port)) as server:
+        while True:
+            connection, client_address = server.accept()
+            with connection:
+                _ = connection.recv(1024)
+                resp = "HTTP/1.1 200 OK\r\n\r\n"
+                print(f"{client_address[0]}:{client_address[1]}: {resp}")
+                connection.sendall(resp.encode())
+
 
 
 if __name__ == "__main__":
