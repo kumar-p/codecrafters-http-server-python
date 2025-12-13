2be75263be6599132846add284b899ed1ac7cc56
Kumar <pradeep-kumar@outlook.com>
2025-12-14 00:55:08 +0530

    return response body

--- app/main.py
+++ app/main.py
@@ -16,8 +16,11 @@ def main():
                 req_line, _, _ = request.partition("\r\n")
                 url = req_line.split(" ")[1]
                 resp = "HTTP/1.1 404 Not Found\r\n\r\n"
-                if url == "/":
-                    resp = "HTTP/1.1 200 OK\r\n\r\n"
+                if url.startswith("/echo/"):
+                    url_param = url.split("/")[-1]
+                    content_length = len(url_param)
+                    headers = f"Content-type: text/plain\r\nContent-Length: {content_length}"
+                    resp = f"HTTP/1.1 200 OK\r\n{headers}\r\n\r\n{url_param}"
                 print(f"\nSending the response:\n{resp}")
                 connection.sendall(resp.encode())
 
