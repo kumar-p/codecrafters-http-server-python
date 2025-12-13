1fafb88c86cc255ee4eeabf5d9f14ac41f57704c
Kumar <pradeep-kumar@outlook.com>
2025-12-14 00:34:15 +0530

    url based response

--- app/main.py
+++ app/main.py
@@ -2,19 +2,23 @@ import socket  # noqa: F401
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     host = "localhost"
     port = 4221
 
     with socket.create_server((host, port)) as server:
         while True:
             connection, client_address = server.accept()
+            print("Connected by", client_address)
             with connection:
-                _ = connection.recv(1024)
-                resp = "HTTP/1.1 200 OK\r\n\r\n"
-                print(f"{client_address[0]}:{client_address[1]}: {resp}")
+                req = connection.recv(1024)
+                request = req.decode()
+                print(f"received the request:\n{request.strip()}")
+                req_line, _, _ = request.partition("\r\n")
+                url = req_line.split(" ")[1]
+                resp = "HTTP/1.1 404 Not Found\r\n"
+                if url == "/":
+                    resp = "HTTP/1.1 200 OK\r\n\r\n"
+                print(f"\nSending the response:\n{resp}")
                 connection.sendall(resp.encode())
 
 
